7d77795c4b72b26c8ef62b756b051d903413d697
Matthew Albrecht <mattparks5855@gmail.com>
2014-06-12 06:51:19 -0500

    Update cape system

--- src/minecraft/mattparks/mods/MattparksCore/EventCapeRender.java
+++ src/minecraft/mattparks/mods/MattparksCore/EventCapeRender.java
@@ -22,7 +22,7 @@ import cpw.mods.fml.common.Loader;
 
 public class EventCapeRender
 {
-    private final String serverLocation = "https://raw.githubusercontent.com/4-Space/Mattparks-Core-2.0/master/capes.txt";
+    private final String serverLocation = "https://github.com/4-Space/MattparksCapes/master/capes.txt";
     private final int timeout = 1000;
 
     private static final Graphics TEST_GRAPHICS = new BufferedImage(128, 128, BufferedImage.TYPE_INT_RGB).getGraphics();
--- src/minecraft/mattparks/mods/Version.java
+++ src/minecraft/mattparks/mods/Version.java
@@ -5,7 +5,7 @@ public class Version
 	public static final int LOCALMAJVERSION = 2;
 	public static final int LOCALMINVERSION = 0;
 	public static final int LOCALBUILDVERSION = 5;
-	public static final int LOCALGITVERSION = 14; //UPDATES EVERY BUILD
+	public static final int LOCALGITVERSION = 15; //UPDATES EVERY BUILD
 	public static int remoteMajVer;
 	public static int remoteMinVer;
 	public static int remoteBuildVer;
