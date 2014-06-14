a40428a744f28b860c45759d01d145f71c9ac346
Matthew Albrecht <mattparks5855@gmail.com>
2014-06-14 14:31:06 -0500

    Update LICENSE and mcmod.info's

--- src/minecraft/mattparks/mods/MattparksCore/Version.java
+++ src/minecraft/mattparks/mods/MattparksCore/Version.java
@@ -4,8 +4,8 @@ public class Version
 {
 	public static final int LOCALMAJVERSION = 2;
 	public static final int LOCALMINVERSION = 0;
-	public static final int LOCALBUILDVERSION = 5;
-	public static final int LOCALGITVERSION = 16; //UPDATES EVERY BUILD
+	public static final int LOCALBUILDVERSION = 6;
+	public static final int LOCALGITVERSION = 17; //UPDATES EVERY BUILD
 	public static int remoteMajVer;
 	public static int remoteMinVer;
 	public static int remoteBuildVer;
