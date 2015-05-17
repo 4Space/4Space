c49a6077c3fc1be589658b69c0d3f7c69e593cb1
Matthew Albrecht <mattparks5855@gmail.com>
2015-05-17 17:09:49 -0500

    Start Exoplanets 4Space Addon.

---
+++ src/main/java/mattparks/mods/space/exoplanets/Constants.java
@@ -0,0 +1,13 @@
+package mattparks.mods.space.exoplanets;
+
+public class Constants {
+	public static final int LOCALMAJVERSION = 0;
+	public static final int LOCALMINVERSION = 0;
+	public static final int LOCALBUILDVERSION = 1;
+
+	public static final String VERSION = (LOCALMAJVERSION + "." + LOCALMINVERSION + "." + LOCALBUILDVERSION);
+
+	/* Core */
+	public static final String MOD_ID_CORE = "ExoPlanets";
+	public static final String MOD_NAME_CORE = "4Space ExoPlanets";
+}
